ad79f739e0cfeb8b48c63585a1bd840dc3366094
KicsiBobby <furdanmilan@gmail.com>
2024-02-13 15:00:49 +0100

    valami

--- src/main/java/com/example/keringo/HelloController.java
+++ src/main/java/com/example/keringo/HelloController.java
@@ -1,8 +1,23 @@
 package com.example.keringo;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 public class HelloController {
+
+    @FXML private Slider sdSpeed;
+    @FXML private Slider sdRadius;
+    @FXML private Label lbSpeed;
+    @FXML private Label lbRadius;
+
+    public void initialize(){
+        lbSpeed.textProperty().bind(Bindings.format("Speed: %0.f",sdSpeed.valueProperty()));
+        lbRadius.textProperty().bind(Bindings.format("Radius: %0.f",sdRadius.valueProperty()));
+    }
+
+
+
 
 }
